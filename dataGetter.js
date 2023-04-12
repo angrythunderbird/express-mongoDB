cdf735f8e7a8c574d92dd92a418d4c56d8c9c96b
angrythunderbird <headcrab.albert@gmail.com>
2023-04-12 19:57:06 +0300

    feat: add users get & create handlers

--- dataGetter.js
+++ dataGetter.js
@@ -5,6 +5,8 @@ module.exports = class {
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
   );
 
+  users = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/users.json`));
+
   getAllTours = (req, res) => {
     res.status(200).json({
       status: 'success',
@@ -159,9 +161,52 @@ module.exports = class {
   };
 
   getAllUsers = (req, res) => {
-    res.status(500).json({
-      status: 'error',
-      message: 'No request handler yet',
+    res.status(200).json({
+      status: 'success',
+      results: this.users.length,
+      data: {
+        users: this.users,
+      },
     });
+  };
+
+  getUserByID = (req, res) => {
+    const id = req.params.id * 1;
+    const user = this.users[id - 1];
+
+    if (!user) {
+      return res.status(404).json({
+        status: 'fail',
+        message: 'Invalid ID',
+      });
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        user,
+      },
+    });
+  };
+
+  createUser = (req, res) => {
+    const newId = this.users[this.users.length - 1].id + 1;
+
+    const newUser = { id: newId, ...req.body };
+
+    this.users.push(newUser);
+
+    fs.writeFile(
+      `${__dirname}/dev-data/data/users.json`,
+      JSON.stringify(this.users),
+      (err) => {
+        res.status(201).json({
+          status: 'success',
+          data: {
+            user: newUser,
+          },
+        });
+      }
+    );
   };
 };
